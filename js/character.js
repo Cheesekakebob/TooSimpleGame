a60452f5ea29fd91f88df522366dd177640676c4
Cheesekakebob <jjm494@yahoo.com>
2016-08-28 03:06:49 -0600

    Update character.js

--- js/character.js
+++ js/character.js
@@ -3,28 +3,41 @@ var Character = function (inputname) {
 
   this.name = inputname;
 
-  this.health = 10;
-
+  this.maxHealth = 10;
+  this.currentHealth = this.maxHealth;
   this.attackDmg = 2;
 
+  this.deaths = 0;
 };
-
 
 
 Character.prototype.sayHello = function() {
 
-  console.log("Hello, I'm " + this.name + ". I have " + this.health + " health and do " + this.attackDmg + " damage!");
+  logMessage("User Character: Hello, I'm " + this.name + ". I have " + this.maxHealth + " health and do " + this.attackDmg + " damage!");
 
 };
 
 
 
-Character.prototype.attack = function() {
-  console.log("Attack made for " + this.attackDmg + "!");
+Character.prototype.takeDamage = function(damage) {
+  this.currentHealth -= damage;
+  characterHealthbar.value = this.currentHealth;
+  characterHealthText.innerText = myCharacter.currentHealth + "/" + myCharacter.maxHealth;
+  if (this.currentHealth <= 0) {
+    this.deaths += 1;
+    characterDeathsText.innerText = this.deaths;
+    this.currentHealth = this.maxHealth;
+    characterHealthbar.value = this.currentHealth;
+    characterHealthText.innerText = myCharacter.currentHealth + "/" + myCharacter.maxHealth;
+    logMessage("Game Message: You've been fatally wounded! That's " + this.deaths + " times! Here's a full heal!");
+  }
 };
 
-var myCharacter = new Character("Batman");
 
-
-
-myCharacter.sayHello();
+Character.prototype.attack = function(target) {
+  logMessage("User Character: Attack made for " + this.attackDmg + " damage against " + target.name + "!");
+  var killed = myEnemy.takeDamage(this.attackDmg);
+  if (!killed) {
+    myEnemy.attack(myCharacter);
+  }
+};
