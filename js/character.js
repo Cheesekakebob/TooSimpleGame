876c28ada1f9cdcb7a106f11d5e0a7c41bdecf8d
Cheesekakebob <jjm494@yahoo.com>
2016-08-28 01:02:27 -0600

    Update character.js

--- js/character.js
+++ js/character.js
@@ -1,14 +1,30 @@
 
 var Character = function (inputname) {
+
   this.name = inputname;
+
   this.health = 10;
+
   this.attackDmg = 2;
+
 };
 
+
+
 Character.prototype.sayHello = function() {
+
   console.log("Hello, I'm " + this.name + ". I have " + this.health + " health and do " + this.attackDmg + " damage!");
+
+};
+
+
+
+Character.prototype.attack = function() {
+  console.log("Attack made for " + this.attackDmg + "!");
 };
 
 var myCharacter = new Character("Batman");
+
+
 
 myCharacter.sayHello();
