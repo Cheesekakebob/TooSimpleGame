76802438fd40e41627df1f481b9816757b7e2b61
Cheesekakebob <jjm494@yahoo.com>
2016-08-28 03:07:06 -0600

    Update enemy.js

--- js/enemy.js
+++ js/enemy.js
@@ -1,13 +1,41 @@
+
 var Enemy = function (inputname) {
+
   this.name = inputname;
-  this.health = 4;
+
+  this.maxHealth = 5;
+  this.currentHealth = this.maxHealth;
   this.attackDmg = 1;
+
+  this.deaths = 0;
 };
+
 
 Enemy.prototype.sayHello = function() {
-  console.log("Grrr, I'm " + this.name + ". I have " + this.health + " health and do " + this.attackDmg + " damage!");
+
+  logMessage("NPC Enemy: Hello, I'm " + this.name + ". I have " + this.maxHealth + " health and do " + this.attackDmg + " damage!");
+
 };
 
-var myEnemy = new Enemy("Joker");
 
-myEnemy.sayHello();
+
+Enemy.prototype.takeDamage = function(damage) {
+  this.currentHealth -= damage;
+  enemyHealthbar.value = this.currentHealth;
+  enemyHealthText.innerText = myEnemy.currentHealth + "/" + myEnemy.maxHealth;
+  if (this.currentHealth <= 0) {
+    this.deaths += 1;
+    enemyDeathsText.innerText = this.deaths;
+    this.currentHealth = this.maxHealth;
+    enemyHealthbar.value = this.currentHealth;
+    enemyHealthText.innerText = myEnemy.currentHealth + "/" + myEnemy.maxHealth;
+    logMessage("Game Message: " + this.name + " has been killed! That's " + this.deaths + " times! Here comes another one!");
+    return true;
+  }
+  return false;
+};
+
+Enemy.prototype.attack = function(target) {
+  logMessage("NPC Enemy: Attack made for " + this.attackDmg + " damage against " + target.name + "!");
+  myCharacter.takeDamage(this.attackDmg);
+};
