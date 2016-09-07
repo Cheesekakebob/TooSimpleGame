1d9e6d066a2cf574f5258dcd66dcaf54a0e3ea55
Cheesekakebob <jjm494@yahoo.com>
2016-09-07 16:04:57 -0600

    Add files via upload

--- js/character.js
+++ js/character.js
@@ -6,57 +6,88 @@ var Character = function (type, inputname, maxhp, atkdmg) {
   this.currentHealth = this.maxHealth;
   this.attackDmg = atkdmg;
   this.deaths = 0;
+  this.critChance = 10;
+  this.dodgeChance = 5;
+  this.sayHello = function() {logMessage(this.charType + ": Hello, I'm " + this.name + ". I have " + this.maxHealth + " health and do " + this.attackDmg + " damage!");};
   this.takeDamage = function() {console.log("Error, base class call to takeDamage!");};
   this.attack = function() {console.log("Error, base class call to attack!");};
-};
-
-
-Character.prototype.sayHello = function() {
-  logMessage(this.charType + ": Hello, I'm " + this.name + ". I have " + this.maxHealth + " health and do " + this.attackDmg + " damage!");
+  this.setCritChance = function(crit) { this.critChance = crit;};
+  this.critRoll = function() {return (Math.floor((Math.random() * 100) + 1) <= this.critChance) ? 2:1;};
+  this.hitRoll = function(targetDodge) {return (Math.floor((Math.random() * 100) + 1) <= targetDodge) ? 0:1;};
 };
 
 var myHero = new Character("Hero", "Batman", 10, 2);
 var myEnemy = new Character("NPC Enemy", "Joker", 5, 1);
 
+myHero.experience = 0;
+myHero.healAmount = 4;
+myEnemy.expvalue = 2;
+
+myHero.gainExperience = function(exp) {
+  this.experience += exp;
+  logMessage("Gained experience: " + exp);
+  if (this.experience >= 10) {
+    if (this.critChance < 100) {
+      logMessage("Leveled Up! Stats increased!");
+      this.setCritChance(this.critChance+1);
+	  this.experience = 0;
+	} else {
+	  logMessage("Max Level Reached! Stats Maxed!");
+	  this.experience = -9999;
+	}
+  }
+  update();
+};
+
+myHero.healMe = function(heal) {
+  ((this.currentHealth + heal) > this.maxHealth) ? this.currentHealth = this.maxHealth : this.currentHealth += heal;
+  logMessage("Hero: Healed for " + heal + " health!");
+  update();
+  myEnemy.attack(myHero)
+}
+
 myHero.takeDamage = function(damage) {
   this.currentHealth -= damage;
-  heroHealthbar.value = this.currentHealth;
-  heroHealthText.innerText = myHero.currentHealth + "/" + myHero.maxHealth;
   if (this.currentHealth <= 0) {
     this.deaths += 1;
-    heroDeathsText.innerText = this.deaths;
     this.currentHealth = this.maxHealth;
-    heroHealthbar.value = this.currentHealth;
-    heroHealthText.innerText = myHero.currentHealth + "/" + myHero.maxHealth;
     logMessage("Game Message: You've been fatally wounded! That's " + this.deaths + " times! Here's a full heal!");
   }
+  update();
 };
 
 myHero.attack = function(target) {
-  logMessage("User Character: Attack made for " + this.attackDmg + " damage against " + target.name + "!");
-  var killed = myEnemy.takeDamage(this.attackDmg);
-  if (!killed) {
-    myEnemy.attack(myHero);
+  hit = this.hitRoll(myEnemy.dodgeChance);
+  if (hit) {
+    dmg = this.attackDmg*this.critRoll(this.critChance);
+    logMessage("Hero: Attack made for " + dmg + " damage against " + target.name + "!");
+    killed = myEnemy.takeDamage(dmg);
+    (!killed) ? myEnemy.attack(myHero):myHero.gainExperience(myEnemy.expvalue);
+  } else {
+    logMessage("Hero: Attacked but " + myEnemy.name + " dodged!");
+	myEnemy.attack(myHero)
   }
 };
 
 myEnemy.takeDamage = function(damage) {
   this.currentHealth -= damage;
-  enemyHealthbar.value = this.currentHealth;
-  enemyHealthText.innerText = myEnemy.currentHealth + "/" + myEnemy.maxHealth;
   if (this.currentHealth <= 0) {
     this.deaths += 1;
-    enemyDeathsText.innerText = this.deaths;
     this.currentHealth = this.maxHealth;
-    enemyHealthbar.value = this.currentHealth;
-    enemyHealthText.innerText = myEnemy.currentHealth + "/" + myEnemy.maxHealth;
     logMessage("Game Message: " + this.name + " has been killed! That's " + this.deaths + " times! Here comes another one!");
+	update();
     return true;
   }
+  update();
   return false;
 };
 
 myEnemy.attack = function(target) {
-  logMessage("NPC Enemy: Attack made for " + this.attackDmg + " damage against " + target.name + "!");
-  myHero.takeDamage(this.attackDmg);
+  hit = this.hitRoll(myHero.dodgeChance);
+  if (hit) {
+    logMessage(myEnemy.name + ": Attack made for " + this.attackDmg + " damage against " + target.name + "!");
+    myHero.takeDamage(this.attackDmg);
+  } else {
+    logMessage(myEnemy.name + ": Attacked but " + myHero.name + " dodged!");
+  }
 };
--- js/game.js
+++ js/game.js
@@ -14,6 +14,14 @@ function initialize() {
   heroHealthText = document.getElementById("herohealthtext");
   heroHealthText.innerText = myHero.maxHealth + "/" + myHero.maxHealth;
   heroDeathsText = document.getElementById("herodeathstext");
+  heroDamageText = document.getElementById("herodamagetext");
+  heroDamageText.innerText = myHero.attackDmg;
+  heroCritText = document.getElementById("herocrittext");
+  heroCritText.innerText = myHero.critChance;
+  heroDodgeText = document.getElementById("herododgetext");
+  heroDodgeText.innerText = myHero.dodgeChance;
+  heroHealText = document.getElementById("herohealtext");
+  heroHealText.innerText = myHero.healAmount;
 
   enemyHealthbar = document.getElementById("enemyhealthbar");
   enemyHealthbar.value = myEnemy.maxHealth;
@@ -23,6 +31,23 @@ function initialize() {
   enemyHealthText = document.getElementById("enemyhealthtext");
   enemyHealthText.innerText = myEnemy.maxHealth + "/" + myEnemy.maxHealth;
   enemyDeathsText = document.getElementById("enemydeathstext");
+  enemyNameText = document.getElementById("enemynametext");
+  enemyNameText.innerText = myEnemy.name;
+}
+
+function update() {
+  heroHealthbar.value = myHero.currentHealth;
+  heroHealthText.innerText = myHero.currentHealth + "/" + myHero.maxHealth;
+  heroDeathsText.innerText = myHero.deaths;
+  heroDamageText.innerText = myHero.attackDmg;
+  heroCritText.innerText = myHero.critChance;
+  heroDodgeText.innerText = myHero.dodgeChance;
+  heroHealText.innerText = myHero.healAmount;
+  
+  enemyHealthbar.value = myEnemy.currentHealth;
+  enemyHealthText.innerText = myEnemy.currentHealth + "/" + myEnemy.maxHealth;
+  enemyDeathsText.innerText = myEnemy.deaths;
+  enemyNameText.innerText = myEnemy.name;
 }
 
 function logMessage(msg) {
