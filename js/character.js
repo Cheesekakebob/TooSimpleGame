2fbf8d171d87193f0cc3b13f4f15b523e39d0024
Cheesekakebob <jjm494@yahoo.com>
2016-09-06 18:51:43 -0600

    Update character.js

--- js/character.js
+++ js/character.js
@@ -1,43 +1,61 @@
 
-var Character = function (inputname) {
-
+var Character = function (type, inputname, maxhp, atkdmg) {
+  this.charType = type;
   this.name = inputname;
-
-  this.maxHealth = 10;
+  this.maxHealth = maxhp;
   this.currentHealth = this.maxHealth;
-  this.attackDmg = 2;
-
+  this.attackDmg = atkdmg;
   this.deaths = 0;
+  this.takeDamage = function() {console.log("Error, base class!");};
 };
 
 
 Character.prototype.sayHello = function() {
-
-  logMessage("User Character: Hello, I'm " + this.name + ". I have " + this.maxHealth + " health and do " + this.attackDmg + " damage!");
-
+  logMessage(this.charType + ": Hello, I'm " + this.name + ". I have " + this.maxHealth + " health and do " + this.attackDmg + " damage!");
 };
 
+var myHero = new Character("Hero", "Batman", 10, 2);
+var myEnemy = new Character("NPC Enemy", "Joker", 5, 1);
 
-
-Character.prototype.takeDamage = function(damage) {
+myHero.takeDamage = function(damage) {
   this.currentHealth -= damage;
-  characterHealthbar.value = this.currentHealth;
-  characterHealthText.innerText = myCharacter.currentHealth + "/" + myCharacter.maxHealth;
+  heroHealthbar.value = this.currentHealth;
+  heroHealthText.innerText = myHero.currentHealth + "/" + myHero.maxHealth;
   if (this.currentHealth <= 0) {
     this.deaths += 1;
-    characterDeathsText.innerText = this.deaths;
+    heroDeathsText.innerText = this.deaths;
     this.currentHealth = this.maxHealth;
-    characterHealthbar.value = this.currentHealth;
-    characterHealthText.innerText = myCharacter.currentHealth + "/" + myCharacter.maxHealth;
+    heroHealthbar.value = this.currentHealth;
+    heroHealthText.innerText = myHero.currentHealth + "/" + myHero.maxHealth;
     logMessage("Game Message: You've been fatally wounded! That's " + this.deaths + " times! Here's a full heal!");
   }
 };
 
-
-Character.prototype.attack = function(target) {
+myHero.attack = function(target) {
   logMessage("User Character: Attack made for " + this.attackDmg + " damage against " + target.name + "!");
   var killed = myEnemy.takeDamage(this.attackDmg);
   if (!killed) {
-    myEnemy.attack(myCharacter);
+    myEnemy.attack(myHero);
   }
+};
+
+myEnemy.takeDamage = function(damage) {
+  this.currentHealth -= damage;
+  enemyHealthbar.value = this.currentHealth;
+  enemyHealthText.innerText = myEnemy.currentHealth + "/" + myEnemy.maxHealth;
+  if (this.currentHealth <= 0) {
+    this.deaths += 1;
+    enemyDeathsText.innerText = this.deaths;
+    this.currentHealth = this.maxHealth;
+    enemyHealthbar.value = this.currentHealth;
+    enemyHealthText.innerText = myEnemy.currentHealth + "/" + myEnemy.maxHealth;
+    logMessage("Game Message: " + this.name + " has been killed! That's " + this.deaths + " times! Here comes another one!");
+    return true;
+  }
+  return false;
+};
+
+myEnemy.attack = function(target) {
+  logMessage("NPC Enemy: Attack made for " + this.attackDmg + " damage against " + target.name + "!");
+  myHero.takeDamage(this.attackDmg);
 };
